26c8f069d9dc556bce44466a25f9d2882e6ba0e8
edwarfigueroa.GLOB <edwarfigueroa.glob@commandalkon.com>
2025-01-23 12:04:38 -0500

    version 2

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const add = (a, b) => a + b
 const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
+
+const calculator = {
+  add,
+  subtract,
+  multiply,
+  divide,
+}
+module.exports = calculator
